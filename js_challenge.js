a05b0322f60738056979e7fb0be1819406feb128
Nawalab22 <nawalab239@gmail.com>
2023-10-12 17:56:03 +0100

    added for loops ,while ,if/else/of,switch and arrays by heart

--- js_challenge.js
+++ js_challenge.js
@@ -1,1 +1,38 @@
 // write a for loop printing your name 20 times
+// for (let i = 0, i < 20; i ++){
+//  console.log('Nawal')
+// }
+// while loops
+// convert the while loop to a do .. while loop.#
+// let i = 0
+// do{
+//   console.log('Nawal')
+//   i++
+// }
+// while (i < 20)
+// write an array that holds 10 names of your friends and family
+// let friendsAndFamily =['samira','mohamed']
+//write code that checks if today is monday it should say go to school else if today is tuesday go to work else if today is friday go to the mosque
+const today = "Monday";
+if (today == "Monday"){console.log('Go to school')
+  
+} 
+else if (today == "Tuesday"){console.log('Go to work')
+  
+} 
+else  { console.log('go to mosque')
+  
+}
+// convert the if else program above to switch
+switch ("today") {
+  case "monday":
+    console.log("go to school from switch")
+    break;
+    case "tuesday":
+    console.log("go to mosque from switch")
+    break;
+
+  default:
+    console.log("go to mosque from switch")
+    break;
+}
